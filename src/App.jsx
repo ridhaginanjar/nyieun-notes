e1f2ec9176708056f5f3f32f13b365de32ae6a9d
ridhaginanjar <ridhaginanjar7@gmail.com>
2026-07-13 11:23:30 +0700

    feat: styling notes form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import './styles.css';
+
 function Header() {
-    return <h1>Header</h1>
+    return <h1>Nyieun Notes</h1>
 }
 
 function Footer() {
-    return <h1>Footer</h1>
+    return <h1>&copy; 2026 Nyeiun Notes</h1>
 }
 
 function NotesForm() {
-    return <h1>NotesForm</h1>
+    return (
+        <section className="notes-form">
+            <div className="notes-form__content">
+                <h2>What's on your mind?</h2>
+                <p>Remaining Title Characters: 50</p>
+            </div>
+            <form action="">
+                <input type="text" name="title" placeholder="Tell me the topics" id="form-title" />
+                <textarea name="body" id="form-body" placeholder="What are you thinking about?"></textarea>
+
+                <button type="submit">Create</button>
+            </form>
+        </section>
+    );
 }
 
 function NotesList() {
@@ -18,7 +33,6 @@ export default function App ({name}) {
     return (
         <>
             <Header />
-            <h1>Hello {name}</h1>
             <NotesForm />
             <NotesList />
             <Footer />
