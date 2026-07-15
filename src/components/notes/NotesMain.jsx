3c58ea1d5e0751cb8c292bc66270a27c55ccd945
ridhaginanjar <ridhaginanjar7@gmail.com>
2026-07-15 19:55:10 +0700

    feat: add handling on search

--- src/components/notes/NotesMain.jsx
+++ src/components/notes/NotesMain.jsx
@@ -1,12 +1,22 @@
 import NotesForm from './NotesForm';
 import SearchBar from './SearchBar';
 import NotesList from './NotesList';
+import { useState } from 'react';
 
 function NotesMain() {
+    const [query, setQuery] = useState('');
+
+    const handlingQuery = (event) => {
+        // setQuery((query) => [
+        //     e.target.value
+        // ])
+        console.log(event)
+    };
+
     return (
         <main className='body-notes'>  
             <NotesForm/>
-            <SearchBar/>
+            <SearchBar handlingQuery={handlingQuery}/>
             <NotesList/>
         </main>
     )
--- src/components/notes/SearchBar.jsx
+++ src/components/notes/SearchBar.jsx
@@ -1,8 +1,9 @@
-function SearchBar() {
+function SearchBar({ handlingQuery }) {
+
     return (
         <search className='searchBar'>
             <label htmlFor="searchNotes">Find your notes</label>
-            <input type="search" name="searchNotes" id="searchNotes" placeholder="What are you looking for?"/>
+            <input onChange={ handlingQuery } type="search" name="searchNotes" id="searchNotes" placeholder="What are you looking for?"/>
         </search>
     )
 }
