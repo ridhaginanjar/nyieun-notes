0d306356967ea7024643960d3c988abf229666a7
ridhaginanjar <ridhaginanjar7@gmail.com>
2026-07-15 16:13:15 +0700

    feat: add reactive on remaining char form

--- src/components/notes/NotesForm.jsx
+++ src/components/notes/NotesForm.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 
 function NotesForm() {
+    const [title, setTitle] = useState('');
+
+    const maxChar = 50;
+    const remainingChar = maxChar - title.length
+
     return (
         <section className="notes-form">
             <div className="notes-form__content">
                 <h2>What's on your mind?</h2>
-                <p>Remaining Title Characters: 50</p>
+                <p>Remaining Title Characters: {remainingChar}</p>
             </div>
             <form action="" className='notes-form__form'>
-                <input type="text" name="title" placeholder="Tell me the topics" id="form-title" />
+                <input onChange={(event) => setTitle(event.target.value)} type="text" name="title" placeholder="Tell me the topics" id="form-title" />
                 <textarea name="body" id="form-body" placeholder="What are you thinking about?"></textarea>
 
                 <button type="submit">Create</button>
