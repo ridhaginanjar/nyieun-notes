613947757fcbf2c18cc5ad10512f02b8be5a08ed
ridhaginanjar <ridhaginanjar7@gmail.com>
2026-07-16 11:09:04 +0700

    feat: add dynamic data on archive

--- src/components/notes/ArchiveNotes.jsx
+++ src/components/notes/ArchiveNotes.jsx
@@ -1,15 +1,29 @@
 import NotesCard from './NotesCard/NotesCard'
+import { convertToDate } from '../../utils/utils'
 
-function ArchiveNotes() {
+function ArchiveNotes({archiveData}) {
+    const lengthArchiveData = archiveData.length
     return (
         <section className='notes-group'>
             <header className='notes-group__header'>
-                <h3>Archive Notes (2)</h3>
+                <h3>Archive Notes ({lengthArchiveData})</h3>
                 <p>Stored notes that are out of sight but safe.</p>
             </header>
             <div className='notes-group__item'>
-                        <NotesCard/>
-                        <NotesCard/>
+                {
+                    archiveData.map(ard => {
+                        let createdDate = convertToDate(ard.createdAt);
+                        return (
+                            <NotesCard 
+                                key={ard.id}
+                                notesId={ard.id} 
+                                title={ard.title} 
+                                text={ard.text}
+                                date={createdDate}
+                            />
+                        )
+                    })
+                }
             </div>
         </section>
     )
